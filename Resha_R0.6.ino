de5fa16ba4229e69b004f52bf984f9d10376914d
Muhammad-Sayed <muhammad.b.h.sayed@gmail.com>
2017-05-08 23:13:08 +0100

    Testing stepper drivers

--- Resha_R0.6.ino
+++ Resha_R0.6.ino
@@ -1,17 +1,67 @@
-void setup() {
+#define X_STEP  21
+#define X_DIR   20
+#define Y_STEP  0
+#define Y_DIR   1
+#define Z_STEP  3
+#define Z_DIR   2
+#define EN      19
+#define EN_Z    4
+#define X_STOP  12
+#define Y_STOP  9
+#define Z_STOP  8
+
+void setup() 
+{
   Serial1.begin(115200);
+
+  pinMode(X_STEP, OUTPUT);
+  pinMode(X_DIR, OUTPUT);
+  pinMode(Y_STEP, OUTPUT);
+  pinMode(Y_DIR, OUTPUT);
+  pinMode(Z_STEP, OUTPUT);
+  pinMode(Z_DIR, OUTPUT);
+  pinMode(EN, OUTPUT);
+  pinMode(EN_Z, OUTPUT);
+  pinMode(X_STOP, INPUT_PULLUP);
+  pinMode(Y_STOP, INPUT_PULLUP);
+  pinMode(Z_STOP, INPUT_PULLUP);
+
+  digitalWrite(EN, LOW);
+  digitalWrite(EN_Z, LOW);
+
+  digitalWrite(X_DIR, LOW);
+  digitalWrite(Y_DIR, LOW);
+  digitalWrite(Z_DIR, LOW);
 }
 
-void loop() {
-  Serial1.println("Hello World!");
-  delay(1500);
-  if(Serial.available()) {
-    int inByte = Serial.read();
-    Serial1.print("I got ");
-    while(Serial.available()) {
-      Serial1.write(inByte);
-    }
-    Serial1.println(" ");
-    delay(1000);
+void loop() 
+{
+  if(!digitalRead(X_STOP))
+  {
+    digitalWrite(X_STEP, !digitalRead(X_STEP));
+    Serial1.println("Stepping X");
   }
+  if(!digitalRead(Y_STOP))
+  {
+    digitalWrite(Y_STEP, !digitalRead(Y_STEP));
+    Serial1.println("Stepping Y");
+  }
+  if(!digitalRead(Z_STOP))
+  {
+    digitalWrite(Z_STEP, !digitalRead(Z_STEP));
+    Serial1.println("Stepping Z");
+  }
+  delay(5);
+  
+//  Serial1.println("Hello World!");
+//  delay(1500);
+//  if(Serial.available()) {
+//    int inByte = Serial.read();
+//    Serial1.print("I got ");
+//    while(Serial.available()) {
+//      Serial1.write(inByte);
+//    }
+//    Serial1.println(" ");
+//    delay(1000);
+//  }
 }
