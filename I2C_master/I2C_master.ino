59bb986bfdebfd8663d0daf27cac0fe6e6a06283
Muhammad-Sayed <muhammad.b.h.sayed@gmail.com>
2017-05-30 16:45:10 +0100

    testing thermistor on daughter board

--- I2C_master/I2C_master.ino
+++ I2C_master/I2C_master.ino
@@ -5,21 +5,21 @@ void setup() {
   Serial1.begin(115200);  // start serial for output
 }
 
-byte x = 0;
+//byte x = 0;
 
 void loop() {
-  if (Serial1.available())
-  {
-    x = Serial1.read();
-    Wire.beginTransmission(8); // transmit to device #8
-    Wire.write("x is ");        // sends five bytes
-    Wire.write(x);              // sends one byte
-    Wire.endTransmission();    // stop transmitting
-  }
+//  if (Serial1.available())
+//  {
+//    x = Serial1.read();
+//    Wire.beginTransmission(8); // transmit to device #8
+//    Wire.write("x is ");        // sends five bytes
+//    Wire.write(x);              // sends one byte
+//    Wire.endTransmission();    // stop transmitting
+//  }
 
-  delay(1000);
+  delay(2500);
   
-  Wire.requestFrom(8, 12);    // request 6 bytes from slave device #8
+  Wire.requestFrom(61, 1);    // request 6 bytes from slave device #8
   while (Wire.available()) { // slave may send less than requested
     char c = Wire.read(); // receive a byte as character
     Serial1.print(c);         // print the character
